539f7d0dea620958d912a7668e1ecb5b248c7707
dionenak <dionenak@outlook.com>
2023-07-08 17:53:03 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
 import express, { NextFunction,Request, Response } from "express";
-// import router from "./router/api"
+
 // set up our express app
 const app = express();
-
+// set the port
+const port=8080
+// how to handle get
 app.get('/api', (req, res) => res.send('Its working!'));
 
-
-app.use(express.static('public'));
-
- app.use(express.json());
-// initialize routes
-//  app.use('/api',router);
-
-// error handling middleware
-app.use(function(err:Error,req:Request,res:Response,next:NextFunction){
-    //console.log(err);
-    res.status(422).send({error: err.message});
-});
-
 // listen for requests
-app.listen(process.env.port || 4000, function(){
+app.listen(port, function(){
     console.log('Ready to Go!');
 });
