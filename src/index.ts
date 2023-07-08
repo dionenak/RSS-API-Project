30954814e289543407ee26dc1f5c3267e8928758
dionenak <dionenak@outlook.com>
2023-07-08 20:21:13 +0200

    fix get

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { NextFunction,Request, Response } from "express";
+import express, { Request, Response } from "express";
+import * as fs from 'fs'
 
 // set up our express app
 const app = express();
 // set the port
 const port=8080
 // how to handle get
-app.get('/api', (req, res) => res.send('Its working!'));
+app.get('/api', (req, res) =>{
+    const file=fs.readFileSync('data/feed.xml', 'utf8')
+    // we want to return rss xml
+    res.type('.rss')
+    // If we want to send the file, we could use : res.download('data/feed.xml')
+    res.send(file)
+    });
 
 // listen for requests
 app.listen(port, function(){
