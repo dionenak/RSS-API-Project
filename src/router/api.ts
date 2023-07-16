a0679e3be00ad96573dbc3ce7476e6810befbef0
dionenak <dionenak@outlook.com>
2023-07-16 18:03:12 +0200

    write output

--- src/router/api.ts
+++ src/router/api.ts
@@ -1,28 +1,23 @@
 import express from "express";
-import * as fs from 'fs';
-import * as data from '../../data/input/data.json'
-import {createRSS }from '../controller/routeController'
+import * as fs from "fs";
+import * as data from "../../data/input/data.json";
+import { createRSS } from "../controller/routeController";
 
 const router = express.Router();
 
-router.get('/api',(req, res) =>{
-    const file=fs.readFileSync('data/feed.xml', 'utf8')
+router.get("/api", (req, res) => {
+	const file = fs.readFileSync("data/feed.xml", "utf8");
+	res.type(".rss").send(file);
+});
 
-    res.type('.rss').send(file)
-     })
+router.post("/api", (req, res) => {
+	// We need some error handling.
+	const url = req.body.url;
+	const title = req.body.title;
+	const RSS = createRSS(data.items);
+	fs.writeFileSync("data/output/feed.xml", RSS);
 
-
-router.post('/api',(req, res) =>{
-        // We need some error handling.
-        const url=req.body.url
-        const title=req.body.title
-        const RSS=createRSS(data.items)
-        res.send(`Got ${title},${url},${RSS}`)
-        })
+	res.send(`Got ${title},${url},${RSS}`);
+});
 
 export default router;
-
-
-
-    
-      
