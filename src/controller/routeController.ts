86c1649c4809dc9b1688a290644490257727895f
dionenak <dionenak@outlook.com>
2023-10-23 20:17:18 +0200

    small cleanup-wip

--- src/controller/routeController.ts
+++ src/controller/routeController.ts
@@ -8,7 +8,7 @@ const data: dataType = {
 	items: [],
 };
 
-function createRSS(items: Array<UserInput>): string {
+function instantiateRSS() {
 	/**
 	 * Info we want from config for our XML.
 	 */
@@ -18,11 +18,10 @@ function createRSS(items: Array<UserInput>): string {
 		description: string;
 		lang: "string";
 	} = config.get("project");
-
 	/**
 	 * Constructing the XML object.
 	 */
-	const xmlObject = {
+	const initialXmlObject = {
 		rss: [
 			{
 				_attr: {
@@ -35,16 +34,23 @@ function createRSS(items: Array<UserInput>): string {
 					{ link: projectInfo.link },
 					{ description: projectInfo.description },
 					{ language: projectInfo.lang },
-
-					...items.map((item) => {
-						return {
-							item: [{ title: item.title }, { link: item.link }],
-						};
-					}),
 				],
 			},
 		],
 	};
+
+	return initialXmlObject;
+}
+
+function createRSS(items: Array<UserInput>): string {
+	let xmlObject = instantiateRSS();
+	const objectToAdd = data.items.map((item) => {
+		return {
+			item: [{ title: item.title }, { link: item.link }],
+		};
+	});
+	console.log(objectToAdd);
+	// xmlObject.rss[1].channel![0];
 
 	const xmlString =
 		'<?xml version="1.0" encoding="UTF-8"?>' + xml(xmlObject, { indent: "  " });
@@ -52,24 +58,19 @@ function createRSS(items: Array<UserInput>): string {
 	return xmlString;
 }
 
-export function updateItems(userInput: UserInput, RSSinput: Array<UserInput>) {
+export function updateItems(
+	userInput: UserInput,
+	RSSinput: Array<UserInput>
+): string {
 	let foundItem = false;
 	let foundLink = false;
-
 	for (const item of RSSinput) {
 		if (item.title === userInput.title) {
 			item.link = userInput.link;
 			foundItem = true;
 			break;
 		}
-		// 3. mallon urls den exoume dipla. ama iparxei to kanei skip.
-		if (item.link === userInput.link) {
-			foundLink = true;
-			break;
-		}
 	}
-	// Isws edw na girna to RSS?
-	if (!foundLink) return;
 	if (!foundItem) {
 		RSSinput.push({ title: userInput.title, link: userInput.link });
 	}
